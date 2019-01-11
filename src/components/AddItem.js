d3da40a79f3435b5337924cdaee3eb85bb186036
radeshshiwlochan <radesh0430@gmail.com>
2019-01-10 21:32:49 -0500

    adds functionality for error checking if user enters empty string for an item

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -3,8 +3,10 @@ import React from 'react';
 export default class AddItem extends React.Component {
   handleSubmit = (e) => {
   	e.preventDefault();
-  	const item = e.target.elements.todo.value.trim();
-  	this.props.addItem(item);
+		const item = e.target.elements.todo.value.trim();
+		if (item != '') {
+      this.props.addItem(item);
+		} 
   	e.target.elements.todo.value = '';
   }
   
